a04731225be6d73a54c73f3131073d117f2db4b9
AILINGANGEL <angel20081018@163.com>
2017-08-05 16:23:29 +0800

    2分查找

---
+++ BinarySearch.py
@@ -0,0 +1,18 @@
+
+class BinarySearch:
+    def search(self, array, k):
+        lo = 0
+        hi = len(array) -1
+        while lo <= hi:
+            mid = (lo + hi) /2
+            if array[mid] == k:
+                return mid
+            elif array[mid] < k:
+                lo = mid + 1
+            else:
+                hi = mid -1
+        return -1
+
+test = [1,2,3,4,5]
+binarySearch = BinarySearch()
+print binarySearch.search(test, 3)
