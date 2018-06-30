0fc1a07236df9d9f701c5469ab120f12c85d5f7d
ailingangel <angel20081018@163.com>
2018-06-30 20:59:05 +0800

    去掉不必要的代码

--- IsValidSudoku.js
+++ IsValidSudoku.js
@@ -3,7 +3,7 @@ var blockNo = function(x, y) {
         if (y < 3) return 1;
         if (y < 6) return 2;
         if (y < 9) return 3;
-    } else if ( x < 6) {
+    } else if (x < 6) {
         if (y < 3) return 4;
         if (y < 6) return 5;
         if (y < 9) return 6;
@@ -16,14 +16,16 @@ var blockNo = function(x, y) {
 
 var isValidSudoku = function(board) {
     var map = {};
-    for(var i=0; i<board.length; i++) {
-        for(var j=0; j<board[i].length; j++) {
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[i].length; j++) {
             let num = board[i][j];
-            if (num  !== '.') {
-               if (map[num] === undefined) {
-                    map[num] = [[i, j]];
+            if (num !== '.') {
+                if (map[num] === undefined) {
+                    map[num] = [
+                        [i, j]
+                    ];
                 } else {
-                    for(let k = 0; k < map[num].length; k++) {
+                    for (let k = 0; k < map[num].length; k++) {
                         if (i === map[num][k][0]) {
                             return false;
                         }
@@ -31,16 +33,14 @@ var isValidSudoku = function(board) {
                             return false;
                         }
 
-                        if ( Math.abs(i - map[num][k][0]) < 3 && Math.abs(j - map[num][k][1]) < 3 ) {
-                            if (blockNo(i, j) === blockNo(map[num][k][0], map[num][k][1])) {
-                                return false;
-                            }
+                        if (blockNo(i, j) === blockNo(map[num][k][0], map[num][k][1])) {
+                            return false;
                         }
                     }
                     map[num].push([i, j]);
                 }
             }
-        }//for
-    }//for
+        } //for
+    } //for
     return true;
 };
