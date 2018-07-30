35d938563671dd8928c06a4e60ef147660564bf8
ailingangel <angel20081018@163.com>
2018-07-30 19:39:53 +0800

    链表，删除距离队尾的第n个结点

--- linkList/removeNthFromEnd.js
+++ linkList/removeNthFromEnd.js
@@ -19,3 +19,25 @@ var removeNthFromEnd = function(head, n) {
     }
     return head;
 };
+
+//一次循环
+var removeNthFromEnd = function(head, n) {
+    if (n === 0) return head;
+    var fast = head;
+    var slow = head;
+    while (n != 0) {
+        fast = fast.next;
+        n--;
+    }
+    //如果经过n次迭代之后fast指向null,说明要删除头结点
+    if (fast === null) {
+        return head.next;
+    }
+    // fast指向最后一个结点的时候， slow的距离恰好和他是n,
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    slow.next = slow.next.next;
+    return head;
+};
