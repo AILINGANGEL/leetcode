11758efa9dfd3f3e32aff9f3489120fb7aefae6a
ailing.hou <ailing.hou@nio.com>
2018-07-31 18:18:44 +0800

    再次联系链表反转非递归方法

--- linkList/ReverseLinkList.js
+++ linkList/ReverseLinkList.js
@@ -1,13 +1,13 @@
 var reverseList = function(head) {
     if (head && head.next) {
-       let cur = head.next;
-       head.next = null;
-       while(cur) {
-           let next = cur.next;
-           cur.next = head;
-           head = cur;
-           cur = next;
-       }
+        let cur = head.next;
+        head.next = null;
+        while (cur) {
+            let next = cur.next;
+            cur.next = head;
+            head = cur;
+            cur = next;
+        }
     }
     return head;
 };
@@ -15,10 +15,25 @@ var reverseList = function(head) {
 
 // leetcode 递归解法
 var reverseList = function(head) {
-  if (head === null || head.next === null) 
-    return head;
-  var p = reverseList(head.next);
-  head.next.next = head;
-  head.next = null;
-  return p;
+    if (head === null || head.next === null)
+        return head;
+    var p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
 }
+
+// 2018-7-31-再次练习
+var reverseList = function(head) {
+    if (head === null) return head;
+    var cur = head.next;
+    var next;
+    head.next = null;
+    while (cur) {
+        next = cur.next;
+        cur.next = head;
+        head = cur;
+        cur = next;
+    }
+    return head;
+};
