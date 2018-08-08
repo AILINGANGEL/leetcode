40f66239b7d05d13dae1121624c2d93060a14fb3
ailingangel <angel20081018@163.com>
2018-08-08 22:16:31 +0800

    数组加1再练习

--- Array/PlusOne.js
+++ Array/PlusOne.js
@@ -23,3 +23,16 @@ var plusOne = function(digits) {
     digits.unshift(1);
     return digits;
 };
+
+// 再练习
+var plusOne = function(digits) {
+    for(let i = digits.length - 1; i>=0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
+};
