231cd485381804364b0fb3e2feff86e1491ddb45
ailingangel <angel20081018@163.com>
2018-08-12 18:25:00 +0800

    双指针法

--- Array/findMaxConsecutiveOnes.js
+++ Array/findMaxConsecutiveOnes.js
@@ -14,3 +14,22 @@ var findMaxConsecutiveOnes = function(nums) {
     result = result > num ? result : num;
     return result;
 };
+
+
+// 双指针
+var findMaxConsecutiveOnes = function(nums) {
+    let result = 0;
+    let i = 0;
+    while(i < nums.length) {
+        let j = i;
+        let num = 0;
+        while( j < nums.length && nums[j] === 1) {
+            num += 1;
+            j++;
+        }
+        result = result > num ? result : num;
+        i = j;
+        i++;
+    }
+    return result;
+};
