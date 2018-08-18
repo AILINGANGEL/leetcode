1714ccfa7c947042fe1a3d6dda49b505f60a93fc
ailingangel <angel20081018@163.com>
2018-08-18 22:42:51 +0800

    求众数Boyer-Moore Voting算法

--- Array/majorityElement.js
+++ Array/majorityElement.js
@@ -14,3 +14,19 @@ var majorityElement = function(nums) {
     nums.sort((a, b) => a - b);
     return nums[Math.floor(nums.length / 2)];
 };
+
+var majorityElement = function(nums) {
+    let num = 0;
+    let count = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] === num) {
+            count++;
+        }else if (count === 0){
+            num = nums[i];
+            count = 1;
+        } else {
+            count--;
+        }
+    }
+    return num;
+};
