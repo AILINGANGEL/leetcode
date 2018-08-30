ca3f6905299febe7744d1cd4285385ad43c98ea9
ailingangel <angel20081018@163.com>
2018-08-30 18:14:03 +0800

    合并树,循环遍历解法

--- tree/mergeTrees.js
+++ tree/mergeTrees.js
@@ -7,4 +7,31 @@ var mergeTrees = function(t1, t2) {
     return t1;
 };
 
+
+var mergeTrees = function(t1, t2) {
+    if (t1 === null) return t2;
+    var queue = [
+        [t1, t2]
+    ];
+    while (queue.length > 0) {
+        let [node1, node2] = queue.shift();
+        if (node1 === null || node2 === null) {
+            continue;
+        }
+        node1.val += node2.val;
+        if (node1.left === null) {
+            node1.left = node2.left;
+        } else {
+            queue.push([node1.left, node2.left]);
+        }
+
+        if (node1.right === null) {
+            node1.right = node2.right;
+        } else {
+            queue.push([node1.right, node2.right]);
+        }
+    }
+    return t1;
+};
+
 mergeTrees(t1, t2);
