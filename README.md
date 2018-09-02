3d53d2936f11ec26b707540312adc9122374feb2
ailingangel <angel20081018@163.com>
2018-09-02 16:18:51 +0800

    验证二叉搜索树递归and循环遍历

--- README.md
+++ README.md
@@ -105,4 +105,5 @@
 101 [对称二叉树](https://leetcode-cn.com/problems/symmetric-tree/description/)|将树自己和自己进行对比, 自己的左子树等于自己的右子树| easy
 100 [相同的树](https://leetcode-cn.com/problems/same-tree/description/) || easy
 102 [合并二叉树](https://leetcode-cn.com/problems/merge-two-binary-trees/description/)|1.递归解法 2.循环遍历解法|easy
-98 [验证二叉搜索树](https://leetcode-cn.com/problems/validate-binary-search-tree/description/)|中序遍历|medium
+98 [验证二叉搜索树](https://leetcode-cn.com/problems/validate-binary-search-tree/description/)|中序遍历(递归 and 循环遍历)|medium
+94 [二叉树中序遍历](https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/)|1.递归 2.循环遍历| medium
--- tree/isValidBST.js
+++ tree/isValidBST.js
@@ -1,8 +1,9 @@
+// 递归解法
 var isValidBST = function(root) {
     var arr = [];
     inOrderTranverse(root, arr);
-    for(let i = 0; i < arr.length - 1; i++) {
-        if(arr[i] >= arr[i+1]) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] >= arr[i + 1]) {
             return false;
         }
     }
@@ -10,15 +11,40 @@ var isValidBST = function(root) {
 }
 
 var inOrderTranverse = function(root, arr) {
-    if(root) {
+    if (root) {
         inOrderTranverse(root.left, arr);
         arr.push(root.val);
         inOrderTranverse(root.right, arr);
     }
 }
 
+// 循环遍历解法
+var isValidBST = function(root) {
+    let lastVal;
+    let stack = [];
+    let curr = root;
+    while (stack.length > 0 || curr !== null) {
+        while (curr !== null) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        if (lastVal === undefined) {
+            lastVal = curr.val;
+        } else {
+            if (curr.val <= lastVal) {
+                return false;
+            } else {
+                lastVal = curr.val;
+            }
+        }
+        curr = curr.right;
+    }
+    return true;
+}
+
 var test = {
-    val: 2,
+    val: 0,
     left: null,
     right: {
         val: -1,
