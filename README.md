bffdc0a2db39c0300a4faed9ecb24aad220df3e5
ailingangel <angel20081018@163.com>
2018-09-05 19:57:16 +0800

    树的先序遍历

--- README.md
+++ README.md
@@ -108,4 +108,4 @@
 98 [验证二叉搜索树](https://leetcode-cn.com/problems/validate-binary-search-tree/description/)|中序遍历(递归 and 循环遍历)|medium
 94 [二叉树中序遍历](https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/)|1.递归 2.循环遍历| medium
 700 [二叉搜索树中的搜索](https://leetcode-cn.com/problems/search-in-a-binary-search-tree/description/)|1.递归 2.循环遍历| easy
-144 [二叉树的前序遍历](https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/)|1.递归|medium
+144 [二叉树的前序遍历](https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/)|1.递归 2.循环遍历|medium
--- tree/preorderTraversal.js
+++ tree/preorderTraversal.js
@@ -1,13 +1,47 @@
+// var preorderTraversal = function(root) {
+//     let arr = [];
+//     preorderTree(root, arr);
+//     return arr;
+// };
+
+// var preorderTree = function(root, arr) {
+//     if (root){
+//         arr.push(root.val);
+//         preorderTree(root.left, arr);
+//         preorderTree(root.right, arr);
+//     }
+// }
+
 var preorderTraversal = function(root) {
     let arr = [];
-    preorderTree(root, arr);
+    let stack = [];
+    let node = root;
+    while (stack.length > 0 || node) {
+        if (node) {
+            arr.push(node.val);
+            if (node.right) {
+                stack.push(node.right);
+            }
+            node = node.left;
+        } else {
+            node = stack.pop();
+        }
+    }
     return arr;
 };
 
-var preorderTree = function(root, arr) {
-    if (root){
-        arr.push(root.val);
-        preorderTree(root.left, arr);
-        preorderTree(root.right, arr);
+var test = {
+    val: 1,
+    left: null,
+    right: {
+        val: 2,
+        left: {
+            val: 3,
+            right: null,
+            left: null
+        },
+        right: null
     }
 }
+
+console.log(preorderTraversal(test))
