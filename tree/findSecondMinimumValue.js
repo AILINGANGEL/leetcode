1b08b4b754ca94cc04f5bba238e49322006e16b6
ailingangel <angel20081018@163.com>
2018-09-29 18:13:19 +0800

    二叉树中的第二小节点解法2

--- tree/findSecondMinimumValue.js
+++ tree/findSecondMinimumValue.js
@@ -1,23 +1,45 @@
-var findSecondMinimumValue = function(root) {
-    let arrs = [];
-    helper(root, arrs);
-    let min = root.val;
-    let secondMin = Number.MAX_VALUE;
-    arrs.forEach(num => {
-        if (num > min && num < secondMin) {
-            secondMin = num;
-        }
-    });
-    return secondMin === Number.MAX_VALUE ? -1 : secondMin;
-};
+// var findSecondMinimumValue = function(root) {
+//     let arrs = [];
+//     helper(root, arrs);
+//     let min = root.val;
+//     let secondMin = Number.MAX_VALUE;
+//     arrs.forEach(num => {
+//         if (num > min && num < secondMin) {
+//             secondMin = num;
+//         }
+//     });
+//     return secondMin === Number.MAX_VALUE ? -1 : secondMin;
+// };
 
-var helper = function(root, arrs) {
+// var helper = function(root, arrs) {
+//     if (root) {
+//         arrs.push(root.val);
+//         helper(root.left, arrs);
+//         helper(root.right, arrs);
+//     }
+// }
+
+var findSecondMinimumValue = function(root) {
+    let obj = {
+        min: root.val,
+        secondMin: Number.MAX_VALUE
+    };
+    helper(root, obj);
+    return obj.secondMin === Number.MAX_VALUE ? -1 : obj.secondMin;
+
+}
+var helper = function(root, obj) {
     if (root) {
-        arrs.push(root.val);
-        helper(root.left, arrs);
-        helper(root.right, arrs);
+        if (root.val < obj.secondMin && root.val > obj.min) {
+            obj.secondMin = root.val;
+        } else if (root.val === obj.min) {
+            helper(root.left, obj);
+            helper(root.right, obj);
+        }
     }
 }
+
+
 
 var test = {
     val: 2,
