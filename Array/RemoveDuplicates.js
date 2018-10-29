c2975c645680c14017e40ae00d2ac8c91f2256a0
ailingangel <angel20081018@163.com>
2018-10-29 19:42:37 +0800

    从排序数组中移除重复元素练习第二次

--- Array/RemoveDuplicates.js
+++ Array/RemoveDuplicates.js
@@ -1,6 +1,6 @@
 var removeDuplicates = function(nums) {
     var recordObj = {};
-    for(let i=0; i<nums.length;) {
+    for (let i = 0; i < nums.length;) {
         if (nums[i] in recordObj) {
             nums.splice(i, 1);
         } else {
@@ -17,25 +17,38 @@ var removeDuplicates = function(nums) {
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    for(let i=0; i<nums.length -1;) {
-       if (nums[i] == nums[i+1]) {
-           nums.splice(i, 1);
-       } else {
-           i++;
-       }
+    for (let i = 0; i < nums.length - 1;) {
+        if (nums[i] == nums[i + 1]) {
+            nums.splice(i, 1);
+        } else {
+            i++;
+        }
     }
     return nums.length;
 };
 
 //双指针解法
+// var removeDuplicates = function(nums) {
+//     var n = nums.length;
+//     if (n < 2) return n;
+//     var i = 0,
+//         j = 1;
+//     while (j < n) {
+//         if (nums[j] != nums[i]) {
+//             i++;
+//             nums[i] = nums[j];
+//         }
+//         j++;
+//     }
+//     return i + 1;
+// };
+
 var removeDuplicates = function(nums) {
-    var n = nums.length;
-    if (n < 2) return n;
-    var i = 0, j = 1;
-    while(j<n) {
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j];
+    let i = 0;
+    let j = 1;
+    while (j < nums.length) {
+        if (nums[i] !== nums[j]) {
+            nums[++i] = nums[j];
         }
         j++;
     }
