aa47f7a74b1116526e64b2fea0d46b080b30b8d5
ailingangel <angel20081018@163.com>
2018-11-09 23:10:46 +0800

    实现一个hashset

---
+++ HashTable/MyHashSet.js
@@ -0,0 +1,40 @@
+nitialize your data structure here.
+ */
+var MyHashSet = function() {
+    this.set = {};
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    this.set[key] = true;
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    if(this.contains(key)) {
+        delete this.set[key];
+    }
+};
+
+/**
+ * Returns true if this set contains the specified element 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return !!this.set[key];
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = Object.create(MyHashSet).createNew()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
