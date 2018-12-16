87b5b75177d231188af304330622506ec4699064
ailingangel <angel20081018@163.com>
2018-12-16 21:26:58 +0800

    两束之和第三次

---
+++ HashTable/twoSum.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+   let map = {};
+  for(let i = 0; i < nums.length; i++) {
+       if(map[target - nums[i]] === undefined) {
+           map[nums[i]] = i;
+       } else {
+           return [map[target - nums[i]], i];
+       }
+   }
+};
