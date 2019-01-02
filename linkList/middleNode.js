8ecb274606b3301c786e266f710fd28ab2a8b627
ailingangel <angel20081018@163.com>
2019-01-02 20:23:18 +0800

    链表的中间节点 再次练习

--- linkList/middleNode.js
+++ linkList/middleNode.js
@@ -9,3 +9,14 @@ var middleNode = function(head) {
     }
     return slow.next;
 };
+
+var middleNode = function(head) {
+    if(head === null || head.next === null) return head;
+    let fast = head;
+    let slow = head;
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
