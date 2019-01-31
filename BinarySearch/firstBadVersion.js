9c9a987afa42b0f0a7b6525ae6d5402e50cc4d2b
ailingangel <angel20081018@163.com>
2019-01-31 23:50:42 +0800

    第一个错误版本(2)

--- BinarySearch/firstBadVersion.js
+++ BinarySearch/firstBadVersion.js
@@ -6,14 +6,35 @@ var solution = function(isBadVersion) {
     return function(n) {
         let left = 1;
         let right = n;
-        while(left < right) {
-            let mid = Math.floor((left + right)/2);
-            if(isBadVersion(mid)) {
+        while (left < right) {
+            let mid = Math.floor((left + right) / 2);
+            if (isBadVersion(mid)) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
         return left;
+    };
+};
+
+
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let low = 1;
+        let high = n;
+        while (low < high) {
+            let mid = Math.floor((low + high) / 2);
+            if (isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
     };
 };
