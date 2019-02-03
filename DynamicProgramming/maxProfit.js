e7d30507a6fcae22c4eaa82676df6cd19e527959
ailingangel <angel20081018@163.com>
2019-02-03 12:37:18 +0800

    买卖股票的最佳时机(2)

--- DynamicProgramming/maxProfit.js
+++ DynamicProgramming/maxProfit.js
@@ -4,7 +4,7 @@ var maxProfit = function(prices) {
     let min = prices[0];
     for (let i = 1; i < prices.length; i++) {
         min = Math.min(min, prices[i]);
-        res[i] = Math.max(res[i - 1], prices[i] - min, 0);
+        res[i] = Math.max(res[i - 1], prices[i] - min);
     }
     return res[res.length - 1];
 }
@@ -17,8 +17,8 @@ console.log(maxProfit(test));
 var maxProfit = function(prices) {
     let min = Number.MAX_VALUE;
     let profit = 0;
-    for(let i = 0; i < prices.length; i++) {
-        if(prices[i] < min ) {
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < min) {
             min = prices[i];
         } else if (prices[i] - min > profit) {
             profit = prices[i] - min;
