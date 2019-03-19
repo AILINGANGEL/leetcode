d19c3c0f179caf38c448885c443cba4df485467d
ailingangel <angel20081018@163.com>
2019-03-19 19:20:22 +0800

    回文数(2)

--- Math/isPalindrome.js
+++ Math/isPalindrome.js
@@ -19,3 +19,15 @@ var isPalindrome = function(x) {
     }
     return x === half || x === Math.floor(half/10);
 };
+
+var isPalindrome = function(x) {
+    if(x === 0) return true;
+    if(x < 0 || x % 10 === 0) return false;
+    let tmp = x;
+    let newNum = 0;
+    while(tmp){
+        newNum = newNum * 10 + tmp%10;
+        tmp = Math.floor(tmp/10);
+    }
+    return x === newNum;
+};
