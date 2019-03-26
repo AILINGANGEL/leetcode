77c86779948906370489cc2d1b034d8d6f04b9ec
ailingangel <angel20081018@163.com>
2019-03-26 22:44:08 +0800

    increasingBst Relinking

--- tree/increasingBST.js
+++ tree/increasingBST.js
@@ -1,58 +1,4 @@
-// var increasingBST = function(root) {
-//     var arr = [];
-//     helper(root, arr);
-//     let head = null;
-//     let node = {
-//         val: -1,
-//         left: null,
-//         right: null
-//     };
-//     arr.forEach(item=>{
-//         let tmp = {
-//                 val: item,
-//                 left: null,
-//                 right: null
-//             };
-//         if(head === null) {
-//             head = tmp;  
-//         }
-//         node.right = tmp;
-//         node = node.right;
-//     });
-//     return head;
-// };
-
-// var helper = function(root, arr) {
-//     if(root) {
-//         helper(root.left, arr);
-//         arr.push(root.val);
-//         helper(root.right, arr);
-//     }
-// };
-
-var cur;
-
-var increasingBST = function(root) {
-    let node = {
-        val: -1,
-        left: null,
-        right: null
-    }
-    cur = node;
-    inOrder(root);
-    return node.right;
-};
-
-var inOrder = function(root) {
-    if (root) {
-        inOrder(root.left);
-        root.left = null;
-        cur.right = root;
-        cur = root;
-        inOrder(root.right);
-    }
-}
-
+// 终须遍历
 var increasingBST = function(root) {
     let arrNodes = [];
     inorderTranverse(arrNodes, root);
@@ -81,3 +27,22 @@ var inorderTranverse = function(arr, root) {
         inorderTranverse(arr, root.right);
     }
 };
+
+// 一边遍历一边relink
+let node;
+var increasingBST = function(root) {
+    ans = { val: 0, right: null, left: null };
+    node = ans;
+    inorder(root);
+    return ans.right;
+};
+
+var inorder = function(root) {
+    if (root) {
+        inorder(root.left, node);
+        root.left = null;
+        node.right = root;
+        node = root;
+        inorder(root.right, node);
+    }
+}
