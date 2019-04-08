d2e77ed673d6602d09775c1bbcc1c23de43efe82
AILINGANGEL <angel20081018@163.com>
2019-04-08 22:19:12 +0800

    罗马数字转整数

--- Math/romanToInt.js
+++ Math/romanToInt.js
@@ -26,3 +26,30 @@ var romanToInt = function(s) {
 };
 
 console.log(romanToInt("LVIII"))
+
+
+var romanToInt = function(s) {
+    let dict = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    let i = 0;
+    let ans = 0;
+    while(i < s.length - 1) {
+       if(dict[s[i]] < dict[s[i+1]]) {
+            ans -= dict[s[i]];
+        } else {
+            ans += dict[s[i]];
+        }
+       i++;
+    }
+    if(i < s.length) {
+        ans += dict[s[i]];
+    }
+    return ans;
+};
