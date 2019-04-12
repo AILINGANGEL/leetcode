0096263e51a548ff8773a728c7effe35ed29dca8
AILINGANGEL <angel20081018@163.com>
2019-04-12 23:52:14 +0800

    最长公共前缀

--- String/longestCommonPrefix.js
+++ String/longestCommonPrefix.js
@@ -28,3 +28,33 @@ var longestCommonPrefix = function(strs) {
     }
     return common;
 };
+
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0) return '';
+    if(strs.length === 1) return strs[0];
+    let prefix = commonPrefix(strs[0], strs[1]);
+    if(!prefix) return '';
+    let i = 2;
+    while(i < strs.length) {
+        prefix = commonPrefix(prefix, strs[i]);
+        if(!prefix) return '';
+        i++;
+    }
+    return prefix;
+};
+
+var commonPrefix = function(str1, str2) {
+    let prefix = '';
+    let i = 0;
+    let j = 0;
+    while(i < str1.length && j < str2.length) {
+        if(str1[i] === str2[j]) {
+            prefix += str1[i];
+            i++;
+            j++;
+        } else {
+            break;
+        }
+    }
+    return prefix;
+}
