6aeecfe49dd353f58ece10214bac655738165c9c
ailingangel <angel20081018@163.com>
2019-05-08 18:08:14 +0800

    最大子序和

--- DynamicProgramming/maxSubArray.js
+++ DynamicProgramming/maxSubArray.js
@@ -1,11 +1,7 @@
 var maxSubArray = function(nums) {
     let sum = [nums[0]];
-    let max = sum[0];
     for (let i = 1; i < nums.length; i++) {
-        sum[i] = nums[i] + (sum[i - 1] > 0 ? sum[i - 1] : 0);
-        max = Math.max(max, sum[i]);
+        sum[i] = Math.max(sum[i - 1] + nums[i], nums[i]);
     }
-    return max;
+    return Math.max.apply(null, sum);
 };
-
-console.log(maxSubArray([8, -19, 5, -4, 20]))
