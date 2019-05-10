9cc13d29ce064c6c8536ca760c5e933e3567bc4b
ailingangel <angel20081018@163.com>
2019-05-10 18:14:29 +0800

    findPeakElement binary search

--- BinarySearch/findPeakElement.js
+++ BinarySearch/findPeakElement.js
@@ -1,24 +1,20 @@
-// var findPeakElement = function(nums) {
-//     let i = 0;
-//     while (i < nums.length - 1 && nums[i] < nums[i + 1]) {
-//         i++;
-//     }
-//     return i;
-// }
+var findPeakElement = function(nums) {
+    let i = 0;
+    while (i < nums.length - 1) {
+        if (i === 0) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+            i++;
+        }
+        if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) {
+            return i;
+        }
+        i++;
+    }
+    return nums.length - 1;
+};
 
-// var findPeakElement = function(nums) {
-//     let i = 0;
-//     let j = nums.length - 1;
-//     while (i < j) {
-//         let mid = i + Math.floor((j - i) / 2);
-//         if (nums[mid] < nums[mid + 1]) {
-//             i = mid + 1;
-//         } else {
-//             j = mid;
-//         }
-//     }
-//     return i;
-// }
 
 var findPeakElement = function(nums) {
     return helper(nums, 0, nums.length - 1);
@@ -32,9 +28,8 @@ var helper = function(nums, i, j) {
         } else {
             return helper(nums, i, mid);
         }
-    } else {
-        return i;
     }
+    return i;
 }
 
 var nums = [1, 2, 3]
