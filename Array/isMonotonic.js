62ad3a1ecb290c3f6b3a20f62f9abdb86eed0ec9
ailingangel <angel20081018@163.com>
2019-05-25 14:13:51 +0800

    单调数列

--- Array/isMonotonic.js
+++ Array/isMonotonic.js
@@ -1,20 +1,19 @@
 var isMonotonic = function(A) {
-    if(A.length < 3) return true;
-    let i = 1;
-    let flag = A[1] - A[0];
-    while(i < A.length - 1) {
-        let diff = A[i + 1] - A[i];
-        if(flag === 0) {
-            if(diff !== 0 ) {
-                flag = diff;
+    let increasing;
+    for (let i = 0; i < A.length - 1; i++) {
+        if (A[i] < A[i + 1]) {
+            if (increasing === undefined) {
+                increasing = true;
+            } else if (increasing === false) {
+                return false;
             }
-            i++;
-            continue;
+        } else if (A[i] > A[i + 1]) {
+            if (increasing === undefined) {
+                increasing = false;
+            } else if (increasing === true) {
+                return false;
+            }
         }
-        if(diff * flag < 0) {
-            return false;
-        }
-        i++;
     }
     return true;
 };
