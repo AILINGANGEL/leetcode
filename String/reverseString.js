c81a863f448d2656a3712f54d027931411028aec
ailingangel <angel20081018@163.com>
2019-05-26 15:42:25 +0800

    反转字符串(递归)

--- String/reverseString.js
+++ String/reverseString.js
@@ -1,7 +1,7 @@
 var reverseString = function(s) {
     let i = 0;
     let j = s.length - 1;
-    while(i<j){
+    while (i < j) {
         swap(s, i, j);
         i++;
         j--;
@@ -13,3 +13,18 @@ var swap = function(arr, i, j) {
     arr[i] = arr[j];
     arr[j] = temp;
 };
+
+var reverseString = function(s) {
+    helper(s, 0, s.length - 1);
+};
+
+var helper = function(s, i, j) {
+    if (i >= j) {
+        return;
+    }
+    helper(s, i + 1, j - 1);
+    let tmp = s[i];
+    s[i] = s[j];
+    s[j] = tmp;
+    return s;
+}
