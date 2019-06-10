8f2bf2da80fcb22a22b0b1255a87c15ea4ef2f98
ailingangel <angel20081018@163.com>
2019-06-10 10:54:28 +0800

    子数组

--- BackTracking/subsets.js
+++ BackTracking/subsets.js
@@ -1,15 +1,17 @@
 var subsets = function(nums) {
-    if(nums.length === 0) return [];
+    if (nums.length === 0) return [];
     return helper(nums, nums.length - 1);
 };
 
-function helper(nums, index){
-    if(index === -1) {
-        return [[]];
+function helper(nums, index) {
+    if (index === -1) {
+        return [
+            []
+        ];
     }
     let tmp = helper(nums, index - 1);
     let ans = [];
-    for(let i = 0; i < tmp.length; i++) {
+    for (let i = 0; i < tmp.length; i++) {
         ans.push(tmp[i].slice());
         tmp[i].push(nums[index]);
         ans.push(tmp[i]);
