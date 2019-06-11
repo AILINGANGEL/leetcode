1dbeaab808f334460b63433fd89385b15dda738e
ailingangel <angel20081018@163.com>
2019-06-11 18:05:20 +0800

    爬楼梯(3)

--- DynamicProgramming/climbStairs.js
+++ DynamicProgramming/climbStairs.js
@@ -1,21 +1,21 @@
 var climbStairs = function(n) {
-    if(n < 2) {
-        return n;
+    let arr = [1, 2];
+    for (let i = 2; i < n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
     }
-    let step = [];
-    step[1] = 1;
-    step[2] = 2;
-    for(let i =3; i <= n; i++) {
-        step[i] = step[i - 1] + step[i - 2];
-    }
-    return step[n];
+    return arr[n - 1];
 };
 
 
 var climbStairs = function(n) {
-    let arr = [1, 2];
-    for(let i = 2; i< n; i++){
-        arr[i] = arr[i-1] + arr[i-2];
+    let first = 1;
+    let second = 1;
+    for (let i = 2; i < n + 1; i++) {
+        console.log('-----', first, second);
+        let tmp = second;
+        second = first + second;
+        first = tmp;
     }
-    return arr[n-1];
+    return second;
 };
+console.log(climbStairs(3))
