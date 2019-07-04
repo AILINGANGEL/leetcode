1d17e3b992e9707e737bd76f97e123dbf08ebae9
ailingangel <angel20081018@163.com>
2019-07-04 23:37:57 +0800

    二叉树最大深度(迭代)

--- tree/maxDepth.js
+++ tree/maxDepth.js
@@ -2,3 +2,37 @@ var maxDepth = function(root) {
     if (root === null) return 0;
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+   if (root === null) return 0;
+    let queue = [root];
+    let nextLevel = [];
+    let ans = 1;
+    while (queue.length > 0) {
+        let node = queue.shift();
+        if (node.left) {
+            nextLevel.push(node.left);
+        }
+        if (node.right) {
+            nextLevel.push(node.right);
+        }
+        if (queue.length === 0 && nextLevel.length > 0) {
+            ans++;
+            queue = nextLevel;
+            nextLevel = [];
+        }
+    }
+    return ans;
+};
