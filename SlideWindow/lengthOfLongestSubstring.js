8a68a1d5c5b71425ab399b63e40234d340c7bdda
ailingangel <angel20081018@163.com>
2019-07-14 12:11:21 +0800

    Longest Substring Without Repeating Characters

--- SlideWindow/lengthOfLongestSubstring.js
+++ SlideWindow/lengthOfLongestSubstring.js
@@ -1,34 +1,65 @@
-var lengthOfLongestSubstring = function(s) {
-    let container = new Set();
-    let i = 0;
-    let j = 0;
-    let maxLen = 0;
-    while (j < s.length) {
-        if (!container.has(s[j])) {
-            container.add(s[j]);
-            j++;
-            maxLen = Math.max(maxLen, j - i);
-        } else {
-            container.delete(s[i]);
-            i++;
-        }
 
+// 暴力法
+var allUniqueCharactor = function(s, start, end) {
+    let tmp = new Set();
+    for(let i = start; i <= end; i++) {
+        if(tmp.has(s[i])){
+            return false;
+        } else {
+            tmp.add(s[i]);
+        }
     }
-    return maxLen;
+    return true;
+}
+
+var lengthOfLongestSubstring = function(s) {
+    let ans = 0;
+    for(let i = 0; i < s.length; i++) {
+       for(let j = i; j < s.length; j++){
+         if(allUniqueCharactor(s, i, j)){
+            ans = Math.max(ans, j - i + 1);
+         }
+       }
+    }
+    return ans;
 };
 
-var lengthOfLongestSubstring = function(s) {
-    let container = {};
-    let i = 0;
-    let j = 0;
+// 滑动窗口
+var lengthOfLongestSubstring = function(s){
+    let map = {};
+    let i = 0, j = 0;
     let maxLen = 0;
-    while (j < s.length) {
-        if (s[j] in container) {
-            i = Math.max(container[s[j]] + 1, i);
+    while(j < s.length){
+        if(s[j] in map) {
+            // i = map[s[j]] + 1;
+            i = Math.max(map[s[j]] + 1, i); // 'abba'
         }
-        maxLen = Math.max(maxLen, j - i + 1);
-        container[s[j]] = j;
+        maxLen = Math.max(maxLen, j - i + 1); // 'a'
+        map[s[j]] = j;
         j++;
     }
     return maxLen;
-};
+}
+
+// 滑动窗口
+
+var lengthOfLongestSubstring = function(s){
+    let set = new Set();
+    let i = 0, j = 0;
+    let maxLen = 0;
+    while(j < s.length){
+       if(set.has(s[j])) {
+           set.delete(s[i++]);
+       } else {
+           maxLen = Math.max(maxLen, j - i + 1);
+           set.add(s[j]);
+           j++;
+       }
+    }
+    return maxLen;
+}
+
+console.log(lengthOfLongestSubstring('abba'))
+console.log(lengthOfLongestSubstring('aaaa'))
+console.log(lengthOfLongestSubstring('abcabcbb'))
+console.log(lengthOfLongestSubstring('a'))
