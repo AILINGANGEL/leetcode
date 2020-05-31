572823d62c08a0caa64bea480c5152902cfc7076
ailingangel <angel20081018@163.com>
2020-05-31 14:48:48 +0800

    mypow 分治

--- recursion/myPow.js
+++ recursion/myPow.js
@@ -1,12 +1,24 @@
 var myPow = function(x, n) {
-    if(n === 0) {
+    if (n === 0) {
         return 1;
     }
-    if(n < 0) {
-        x = 1/x;
+    if (n < 0) {
+        x = 1 / x;
         n = -n;
     }
-    return n % 2 === 0 ? myPow(x*x, n/2) : x * myPow(x*x, Math.floor(n/2));
+    return n % 2 === 0 ? myPow(x * x, n / 2) : x * myPow(x * x, Math.floor(n / 2));
 };
 
 console.log(myPow(2, 2))
+var myPow = function(x, n) {
+    if (n === 0) {
+        return 1;
+    }
+    x = n < 0 ? 1 / x : x;
+    n = n < 0 ? -n : n;
+    const ans = Math.pow(myPow(x, Math.floor(n / 2)), 2);
+    return n % 2 === 0 ? ans : ans * x;
+};
+
+// divide-and-conquer
+// x的n次方，等同于x的n/2次方得到的结果和自己相乘;注意处理n为基数的情况
