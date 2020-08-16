7edd0a5028ae4b0f3907dd32169f7aa21c134d3f
ailingangel <angel20081018@163.com>
2020-08-16 21:23:38 +0800

    全排列：回溯

--- BackTracking/permute.js
+++ BackTracking/permute.js
@@ -1,71 +1,94 @@
-// var res;
+// // var res;
+// // var permute = function(nums) {
+// //     res = [];
+// //     helper(nums, res);
+// //     return res;
+// // };
+
+// // var helper = function(nums, arr) {
+// //     if (nums.length > 0) {
+// //         let cur = nums[0];
+// //         if (arr.length === 0) {
+// //             arr.push([cur]);
+// //         } else {
+// //             let arrCopy = arr.slice();
+// //             arr.splice(0);
+// //             arrCopy.forEach(item => {
+// //                 for (let i = item.length; i > -1; i--) {
+// //                     let temp = item.slice();
+// //                     temp.splice(i, 0, cur);
+// //                     arr.push(temp);
+// //                 }
+// //             });
+// //         }
+// //         helper(nums.slice(1), arr);
+// //     }
+// // }
+
+
+
 // var permute = function(nums) {
-//     res = [];
-//     helper(nums, res);
+//     let res = [];
+//     helper(nums, 0, [], res);
 //     return res;
+// }
+
+// var helper = function(nums, start, permutation, permutations) {
+//     if (start === nums.length) {
+//         permutations.push(permutation);
+//     } else {
+//         for (let i = 0; i < permutation.length + 1; i++) {
+//             let newPermuation = permutation.slice();
+//             newPermuation.splice(i, 0, nums[start]);
+//             helper(nums, start + 1, newPermuation, permutations);
+//         }
+//     }
+// }
+
+// var permute = function(nums) {
+//     if (nums.length === 0) return [];
+//     return helper(nums, nums.length - 1);
 // };
 
-// var helper = function(nums, arr) {
-//     if (nums.length > 0) {
-//         let cur = nums[0];
-//         if (arr.length === 0) {
-//             arr.push([cur]);
-//         } else {
-//             let arrCopy = arr.slice();
-//             arr.splice(0);
-//             arrCopy.forEach(item => {
-//                 for (let i = item.length; i > -1; i--) {
-//                     let temp = item.slice();
-//                     temp.splice(i, 0, cur);
-//                     arr.push(temp);
-//                 }
-//             });
-//         }
-//         helper(nums.slice(1), arr);
+// var helper = function(nums, index) {
+//     if (index === 0) {
+//         return [
+//             [nums[0]]
+//         ]
 //     }
+//     let tmp = helper(nums, index - 1);
+//     let ans = [];
+//     for (let i = 0; i < tmp.length; i++) {
+//         for (let j = 0; j < tmp[i].length + 1; j++) {
+//             let p = tmp[i].slice();
+//             p.splice(j, 0, nums[index])
+//             ans.push(p);
+//         }
+//     }
+//     return ans;
 // }
 
 
 
-var permute = function(nums) {
-    let res = [];
-    helper(nums, 0, [], res);
-    return res;
-}
 
-var helper = function(nums, start, permutation, permutations) {
-    if (start === nums.length) {
-        permutations.push(permutation);
-    } else {
-        for (let i = 0; i < permutation.length + 1; i++) {
-            let newPermuation = permutation.slice();
-            newPermuation.splice(i, 0, nums[start]);
-            helper(nums, start + 1, newPermuation, permutations);
-        }
-    }
-}
-
-var permute = function(nums) {
-    if (nums.length === 0) return [];
-    return helper(nums, nums.length - 1);
-};
-
-var helper = function(nums, index) {
-    if (index === 0) {
-        return [
-            [nums[0]]
-        ]
-    }
-    let tmp = helper(nums, index - 1);
+var permute = function (nums) {
     let ans = [];
-    for (let i = 0; i < tmp.length; i++) {
-        for (let j = 0; j < tmp[i].length + 1; j++) {
-            let p = tmp[i].slice();
-            p.splice(j, 0, nums[index])
-            ans.push(p);
+    backtrack(nums, 0, [], ans);
+    return ans;
+}
+// [1, 2] [2, 1]
+var backtrack = function(nums, start, permutation, permutations) {
+    if(permutation.length === nums.length) {
+        permutations.push([...permutation]);
+        return;
+    }
+    for(let i = start; i < nums.length; i++) {
+        for(let j = 0; j < permutation.length + 1; j++) {
+            permutation.splice(j, 0, nums[i]);
+            backtrack(nums, i + 1, permutation, permutations);
+            permutation.splice(j, 1);
         }
     }
-    return ans;
 }
 
 var test = [1, 2, 3];
